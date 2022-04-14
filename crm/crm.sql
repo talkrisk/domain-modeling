5f8241d84721969786287bdf416cc967a8ce9f5a
talkrisk <sam.cooper@kellogg.northwestern.edu>
2022-04-14 02:32:47 +0000

    class part 2

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,15 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
-
+DROP TABLE IF EXISTS conatcts;
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+   contact_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   contact_name TEXT,
+   contact_email TEXT,
+   contact_address TEXT,
+   contact_phone TEXT,
+   company_id INTEGER
+);
+
